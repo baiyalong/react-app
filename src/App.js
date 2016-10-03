6d131ca96e0c244c4b558f65dfa41b9f1ba42a9b
bai <baiyalong@gmail.com>
2016-10-03 14:52:16 +0800

    socket

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+import Header from './Header';
+import Fingerprint2 from 'fingerprintjs2';
+import io from 'socket.io-client';
+import server from './server'
 
 class App extends Component {
+  // constructor() {
+  //   super()
+  // }
+  componentDidMount() {
+    var socket = io(server.addr)
+    new Fingerprint2().get(fp => {
+      socket.on('connect', () => {
+        socket.emit('user.connect', { id: fp }, (err, role, works) => {
+          console.log(err, role, works)
+        })
+      })
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <MuiThemeProvider>
+        <Header title='众创' />
+      </MuiThemeProvider>
     );
   }
 }
---
+++ src/server.js
@@ -0,0 +1,3 @@
+export default {
+    addr: 'http://192.168.1.9:30000'
+}
